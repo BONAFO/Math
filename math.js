083c250dec5051f4946c6563cb29ccf9bd9ffa43
BONAFO <91232074+BONAFO@users.noreply.github.com>
2024-07-18 14:02:37 -0300

    Add files via upload

--- math.js
+++ math.js
@@ -1,12 +1,3 @@
-// const a = 12;
-// const b = 9;
-
-const a = 520123213201;
-const b = 1235412311231;
-
-
-
-console.log((a * b).toExponential(1));
 const repeat = (times, string) => {
     return new Array(times + 1).join(string);
 }
@@ -32,6 +23,8 @@ const sumar_enteros = (a, b) => {
     let extra = 0;
     let result = [];
 
+
+
     a = reverse_element(a);
     b = reverse_element(b);
 
@@ -43,7 +36,6 @@ const sumar_enteros = (a, b) => {
         if (dig.toString().length > 1 && b[i + 1] != undefined) {
             dig = dig.toString();
             extra = parseInt(dig.substring(0, dig.length - 1));
-            // result += dig[dig.length - 1];
             result.push(dig[dig.length - 1]);
         } else {
             result.push(dig.toString());
@@ -57,6 +49,7 @@ const sumar_enteros = (a, b) => {
 const multiplicacion_enteros = (nu1, nu2) => {
     let a
     let b
+
     if (nu2.toString().length <= nu1.toString().length) {
         a = nu1
         b = nu2
@@ -65,17 +58,27 @@ const multiplicacion_enteros = (nu1, nu2) => {
         b = nu1
     }
 
+
     a = reverse_element(a);
     b = reverse_element(b);
 
 
+
     const final_sum = []
 
+
+    b = b.trim();
+    a = a.trim()
     for (let d2 = 0; d2 < b.length; d2++) {
         const num1 = b[d2];
         const toSum = [];
+
+
+
+
         for (let d1 = 0; d1 < a.length; d1++) {
             toSum.push((a[d1] * num1).toString() + repeat(d1, '0'));
+
         }
 
 
@@ -91,7 +94,13 @@ const multiplicacion_enteros = (nu1, nu2) => {
             }
 
         }
+
+
+
+
         final_sum.push(result + repeat(d2, '0'))
+
+
 
     }
 
@@ -108,7 +117,8 @@ const multiplicacion_enteros = (nu1, nu2) => {
     }
 
 
-    // console.log(result);
+
+    return result
 
 
 
@@ -119,42 +129,23 @@ const multiplicacion_enteros = (nu1, nu2) => {
 
 
 
-    // for (let d2 = 0; d2 < b.length; d2++) {
-
-    //     const num1 = b[d2];
-    //     const toSum = [];
-    //     for (let d1 = 0; d1 < a.length; d1++) {
-    //         toSum.push((a[d1] * num1).toString() + repeat(d1, '0'));
-    //     }
 
 
-    //     let result = "";
-    //     for (let i = 0; i < toSum.length; i++) {
-    //         if (i == 0) {
-    //             result = toSum[i];
-    //         } else {
-    //             result = sumar_enteros(toSum[i], result)
-    //         }
-
-    //     }
-
-    //     final_sum.push(result + repeat(d2, '0'))
-    // }
-
-    // let result = "";
-    // for (let i = 0; i < final_sum.length; i++) {
-    //     if (i == 0) {
-    //         result = final_sum[i];
-    //     } else {
-    //         result = sumar_enteros(final_sum[i], result)
-    //     }
-
-    // }
-
-    // console.log(result);
 }
 
-multiplicacion_enteros(a, b)
+const reemplazar_ceros = (bt) => {
+
+    let spitto = 0;
+    for (let i = 0; i < bt.length; i++) {
+        if (bt[i] != 0) {
+            spitto = i;
+            break
+        }
+
+    }
+
+    return bt.substring(spitto)
+}
 
 
 const division_enteros = (dividendo, divisor) => {
@@ -165,11 +156,35 @@ const division_enteros = (dividendo, divisor) => {
 
     for (let i = 0; i < dividendo.length; i++) {
         operando += dividendo[i];
-        if (parseInt(operando) >= divisor) {
-            operando = parseInt(operando);
-            cociente += (parseInt(operando / divisor)).toString();
-            resto = (operando % divisor);
+
+        if (BigInt(operando) >= BigInt(divisor)) {
+
+            let bre = 0;
+            let divisor_torest;
+
+            do {
+
+                divisor_torest = multiplicacion_enteros(divisor, bre);
+                if (BigInt(divisor_torest) > BigInt(operando)) {
+                    bre--
+                    divisor_torest = multiplicacion_enteros(divisor, bre);
+                    break
+                }
+                bre++
+
+
+            } while (true);
+
+            console.log(divisor_torest);
+
+            cociente += bre;
+
+            resto = reemplazar_ceros(resta_enteros(operando, divisor_torest));
+
             operando = "" + resto;
+
+
+
 
         } else {
             cociente += "0"
@@ -184,6 +199,65 @@ const division_enteros = (dividendo, divisor) => {
         resto: resto
     }
 
+}
+
+
+
+
+const resta_enteros = (a, b) => {
+
+    const sign = (BigInt(a) < BigInt(b)) ? ('-') : ('');
+
+
+
+
+    if (a.length < b.length) {
+        const toad = b.length - a.length;
+        a = repeat(toad, "0") + a;
+    } else if (b.length < a.length) {
+        const toad = a.length - b.length;
+        b = repeat(toad, "0") + b;
+    }
+
+    let result = [];
+
+
+    if (BigInt(a) < BigInt(b)) {
+        let ap = a;
+
+        a = b;
+        b = ap;
+    }
+
+
+    a = reverse_element(a);
+    b = reverse_element(b);
+
+
+    let extra = 0;
+
+    for (let i = 0; i < a.length; i++) {
+        let aP = parseInt(a[i]);
+        let bP = parseInt(b[i]);
+        let deb = false;
+        if (aP - extra < bP) {
+
+            aP += 10
+            deb = true;
+        }
+        let dig = (aP - extra) - bP;
+
+        extra = (deb) ? (1) : (0);
+        result.push(dig.toString());
+
+
+
+
+    }
+
+
+
+    return `${sign}${ result.reverse().join("")}`
 }
 
 
